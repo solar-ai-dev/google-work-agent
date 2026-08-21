ba523d90dd7be341aac11381c2964e3d32b61804
bonggyulim <161282085+bonggyulim@users.noreply.github.com>
2026-08-22 07:52:47 +0900

    refactor(r3): add failed transition authority

---
+++ src/google_work_agent/domain/execution_attempt/transitions/mark_failed.py
@@ -0,0 +1,22 @@
+"""Definitive execution failure transition authority."""
+
+from google_work_agent.domain.commands import ActionCommand
+from google_work_agent.domain.enums import ActionStatus, ResultCode
+from google_work_agent.domain.errors import InvariantViolationError
+from google_work_agent.domain.results import CommandResult
+
+
+def transition_mark_failed(
+    current_status: ActionStatus,
+    *,
+    current_version: int,
+    expected_version: int,
+    delivery_certainty: str,
+) -> CommandResult[ActionStatus, ActionCommand]:
+    if delivery_certainty != "NOT_SENT":
+        raise InvariantViolationError("MARK_FAILED requires delivery_certainty=NOT_SENT")
+    if expected_version != current_version:
+        return CommandResult(False, ResultCode.VERSION_CONFLICT, current_status, current_version, (), "expected_version does not match current_version")
+    if current_status is not ActionStatus.EXECUTING:
+        return CommandResult(False, ResultCode.STATE_CONFLICT, current_status, current_version, (), "MARK_FAILED requires EXECUTING")
+    return CommandResult(True, ResultCode.TRANSITION_APPLIED, ActionStatus.FAILED, current_version + 1, (ActionCommand.PREPARE_WRITE_RETRY,))
