9d627fdfcd67ad12db8c57ed58c20723690b0994
bonggyulim <161282085+bonggyulim@users.noreply.github.com>
2026-08-23 00:38:12 +0900

    style: format get resource use case

--- src/google_work_agent/application/use_cases/resource_ref/get_resource.py
+++ src/google_work_agent/application/use_cases/resource_ref/get_resource.py
@@ -5,7 +5,11 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
 
-from google_work_agent.application.ports.connector_failure import ConnectorFailureCode, ConnectorOperationFailure, normalize_google_workspace_failure
+from google_work_agent.application.ports.connector_failure import (
+    ConnectorFailureCode,
+    ConnectorOperationFailure,
+    normalize_google_workspace_failure,
+)
 from google_work_agent.ports import GoogleWorkspaceGatewayError
 
 
@@ -26,11 +30,20 @@ class GetResourceHandler:
 
     def __call__(self, query: GetResourceQuery) -> GetResourceResult:
         if query.source != "gmail":
-            raise ConnectorOperationFailure(code=ConnectorFailureCode.NOT_FOUND, detail_code="RESOURCE_SOURCE_NOT_FOUND")
+            raise ConnectorOperationFailure(
+                code=ConnectorFailureCode.NOT_FOUND,
+                detail_code="RESOURCE_SOURCE_NOT_FOUND",
+            )
         try:
-            resource = self.resource_query_service.get_gmail_thread_detail(resource_id=query.resource_id)
+            resource = self.resource_query_service.get_gmail_thread_detail(
+                resource_id=query.resource_id
+            )
         except GoogleWorkspaceGatewayError as error:
             raise normalize_google_workspace_failure(error) from error
         except RuntimeError as error:
-            raise ConnectorOperationFailure(code=ConnectorFailureCode.CONNECTION_UNAVAILABLE, detail_code="RESOURCE_DETAIL_UNAVAILABLE", retryable=True) from error
+            raise ConnectorOperationFailure(
+                code=ConnectorFailureCode.CONNECTION_UNAVAILABLE,
+                detail_code="RESOURCE_DETAIL_UNAVAILABLE",
+                retryable=True,
+            ) from error
         return GetResourceResult(resource=resource)
